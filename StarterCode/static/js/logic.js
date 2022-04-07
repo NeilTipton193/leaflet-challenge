84671c54755cf0cc0ad4fe96a1e32232367513b0
Neil Tipton <njtipt8413@gmail.com>
2022-04-07 14:45:48 -0500

    Update logic.js

---
+++ StarterCode/static/js/logic.js
@@ -0,0 +1,42 @@
+//generate tile layers
+var defaultView = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+//generate greyscale layer
+var greyscaleView = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+
+//generate topo layer
+var topoView = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+});
+
+//generate basemaps object
+let basemaps= {
+    Default: defaultView,
+    GrayScale: greyscaleView,
+    Topographic: topoView
+}
+
+//generate map object
+var wMap = L.map("map", {
+    center: [35.6762, 139.6503],
+    zoom: 5,
+    layers: [defaultView,greyscaleView, topoView]
+})
+
+// add map object to maps
+defaultView.addTo(worldMap);
+
+//add a layer control
+L.control
+    .layers(basemaps)
+    .addTo(worldMap);
